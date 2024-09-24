660e7335074d32417867dbd04b45c5e9dc69b3ad
valen <valentinahernandezciri556@gmail.com>
2024-09-24 11:23:53 -0500

    pagina final

--- dist/public/admin.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("salir").addEventListener("click", function (e) {
-  e.preventDefault(); // Prevenir el comportamiento por defecto del enlace
-
-  // Eliminar la cookie JWT
-  document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-
-  // Redirigir al usuario al inicio ("/")
-  document.location.href = "/";
-});
